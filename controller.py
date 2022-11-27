c36ce6d2b2fba4bb538bc265fc4b9a539cc89b9f
michaelrupp2002 <63470405+michaelrupp2002@users.noreply.github.com>
2022-11-27 03:43:07 -0600

    First iteration of view, main, controller, and words files. Potentially adding reset button.

---
+++ controller.py
@@ -0,0 +1,66 @@
+import random
+from PyQt5.QtWidgets import *
+from PyQt5.Qt import Qt
+from view import *
+
+
+class Controller(QMainWindow, Ui_MainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.__position = 0
+        self.__clue = ""
+        self.__guess = ""
+        self.__word_list = []
+        self.__word_file = open("words.txt")
+        for word in self.__word_file:
+            self.__word_list.append(word.strip().lower())
+        self.__answer = random.choice(self.__word_list)
+        self.__num_of_guesses = 0
+        self.__guessed_correctly = False
+        self.__label_number = 0
+        self.__labels_list = [self.label_1_1, self.label_1_2, self.label_1_3, self.label_1_4, self.label_1_5,
+                              self.label_2_1, self.label_2_2, self.label_2_3, self.label_2_4, self.label_2_5,
+                              self.label_3_1, self.label_3_2, self.label_3_3, self.label_3_4, self.label_3_5,
+                              self.label_4_1, self.label_4_2, self.label_4_3, self.label_4_4, self.label_4_5,
+                              self.label_5_1, self.label_5_2, self.label_5_3, self.label_5_4, self.label_5_5,
+                              self.label_6_1, self.label_6_2, self.label_6_3, self.label_6_4, self.label_6_5,
+                              ]
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Return:
+            if self.__num_of_guesses < 6 and not self.__guessed_correctly:
+                if self.input_word.text().strip().lower() in self.__word_list:
+                    self.__guess = self.input_word.text().strip().lower()
+                    self.input_word.setText("")
+                    self.__num_of_guesses += 1
+                    self.__guessed_correctly = self.process_guess(self.__answer, self.__guess, self.__num_of_guesses)
+                else:
+                    self.label_errors.setText('Word is not in list')
+            if self.__guessed_correctly:
+                self.label_errors.setText('You guessed the word in' + str(self.__num_of_guesses) + 'guesses')
+            if self.__num_of_guesses == 6 and not self.__guessed_correctly:
+                self.label_errors.setText('Out of guesses the word was,' + self.__answer)
+
+    def process_guess(self, answer, guess, num_guesses):
+        self.__position = 0
+        self.__clue = ""
+        for letter in guess:
+            if letter == answer[self.__position]:
+                self.__clue += "G"
+                self.__label_number = (num_guesses * 5) + (self.__position - 5)
+                self.__labels_list[self.__label_number].setText(letter)
+                self.__labels_list[self.__label_number].setStyleSheet("background-color: green;")
+            elif letter in self.__answer:
+                self.__clue += "Y"
+                self.__label_number = (num_guesses * 5) + (self.__position - 5)
+                self.__labels_list[self.__label_number].setText(letter)
+                self.__labels_list[self.__label_number].setStyleSheet("background-color: yellow;")
+            else:
+                self.__clue += "-"
+                self.__label_number = (num_guesses * 5) + (self.__position - 5)
+                self.__labels_list[self.__label_number].setText(letter)
+                self.__labels_list[self.__label_number].setStyleSheet("background-color: gray;")
+            self.__position += 1
+        return self.__clue == "GGGGG"
